b0013e8480b038cba3d187257c4b08903228a3b6
Mark VanLandingham <markvanlan@gmail.com>
2020-01-24 10:31:16 -0600

    Created eslint plugin with fake rule

---
+++ tests/lib/rules/ember-object.js
@@ -0,0 +1,37 @@
+/**
+ * @fileoverview Disallow Ember.Object usage
+ * @author Mark VanLandingham
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/ember-object"),
+
+    RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+ruleTester.run("ember-object", rule, {
+
+    valid: [
+
+        // give me some code that won't trigger a warning
+    ],
+
+    invalid: [
+        {
+            code: "Ember.Object",
+            errors: [{
+                message: "Fill me in.",
+                type: "Me too"
+            }]
+        }
+    ]
+});
